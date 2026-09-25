0a0b28268b4b8e603e5cf8ce155e62c9bce9eb4c
alex-eth <86966117+alex-eth@users.noreply.github.com>
2026-09-25 17:38:26 +0200

    Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        somme();
+    }
+
+    public static void somme() {
+        int deuxiemeEntier = scanner.nextInt();
+        System.out.println("La somme de " + premierEntier + " avec " + deuxiemeEntier + "est egale a " + somme);
+        int somme = premierEntier + deuxiemeEntier;
+        System.out.println("Veuillezsaisirle premier entier");
+        Scanner scanner = new Scanner(System.in);
+        int premierEntier = scanner.nextInt();
+        System.out.println("Veuillezsaisirle deuxième entier");
+    }
+}
+
+
+
+
